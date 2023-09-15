9cf292345c39247b8539673d4610a25239742406
lalit <ls1570427@gmail.com>
2023-09-15 16:46:32 +0530

    work with monog

---
+++ node.js/fileStream.js
@@ -0,0 +1,20 @@
+import fs from "fs";
+// fs.mkdirSync("Lalit");
+// fs.writeFileSync("Lalit/bio.txt","please give respect");
+// fs.appendFileSync("Lalit/bio.txt"," I am Lalit");
+// fs.readFile("Lalit/bio.txt","UTF-8",(err,data)=>{
+    // console.log(data);
+// })
+// fs.rename("Lalit/bio.txt","Lalit/file.txt",(err)=>{
+    // console.log("Rename Done");
+// });
+// fs.rmdir("Lalit",(err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log("Done");
+//     }
+// })
+// fs.rm("./Lalit",{recursive:true})
+
